878197baa756f05e3a74942f4952dba015d4b4bb
sneha_090 <118058904+sneha090j@users.noreply.github.com>
2024-07-24 10:51:34 +0530

    Update Project.jsx

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -108,4 +108,3 @@ const Project = () => {
 };
 
 export default Project;
-// https://plus.unsplash.com/premium_photo-1671599016130-7882dbff302f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cXVvdGVzJTIwd2FsbHBhcGVyfGVufDB8fDB8fHww
